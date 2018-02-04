d02c0cfcc7bdf9da84696eeadbdfeb9b1109aff0
Erik Zettersten <Erik@Zettersten.com>
2018-02-04 02:05:26 -0500

    force ssl loging

--- wp-config.php
+++ wp-config.php
@@ -78,8 +78,6 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
-define('FORCE_SSL_LOGIN', true);
-define('FORCE_SSL_ADMIN', true); 
 
 
 /* That's all, stop editing! Happy blogging. */   
